73f0bf9c3f5799d72c1eb7ba5a8190824ac4bd5e
Hasbunallah Mubarak Abiola <82613627+Hasbunallahmubarak@users.noreply.github.com>
2024-02-12 13:07:24 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# Kool-Form-Pack
+# Kool Form Pack by Hmabiola
+
+Kool Form Pack is a multi-site, that features a black theme video background. It includes 6 HTML pages for login, create an account, under construction count down, password reset, 404 page and contact form page. There is a pop up notify email and slide in out main menu from the right side. Template was sourced from Tooplate (as regard "design"), and development was done by me (Hmabiola).
+
+## Kool form pack website description
+
+#### The Homepage (index.html file)
+
+- The "Homepage page" consist of top menu bar, with properties such as social icons, hamburger menu icon and notify button.
+
+- Below the menu bar is the main content, which shows an under construction countdown and backgroud video reference link.
+
+<hr />
+
+#### The Login page
+
+- The "Login page" also has the top nav bar component similar to the Homepage, so as other webpages, which enables navigation through the website.
+
+-The main contents in the "login page" are form fields for inputing "log in" details
+
+<hr />
+
+#### The Create an account page
+
+- The "create an account page" consist of form fields for users to create new account by providing some information, as specified.
+
+<hr />
+
+#### The password reset page
+
+- The "password reset page" consist of form fields, mainly password fields, for users to provide new login credentials.
+
+<hr />
+
+#### The Contact page
+
+- The "contact form page" consist of form fields for users to send in their messages, opinions and suggestions to the service provider (in this case "kool form pack") for better service delivery.
+
+<hr />
+
+#### The 404 page
+
+- The "404 page" is the landing page for potential errors in page request (relating to the current website). It has link, that enables the user to go back to the "Homepage".
+
+<hr />
+
+## View site at:
+
+- [Kool-Form-Pack](https://hasbunallahmubarak.github.io/Kool-Form-Pack/)
+
+## Technologies Used
+
+- HTML
+- CSS
+- SCSS / SASS
+- JavaScript
+
+  - Count down
+  - Modal Function
+  - Sidebar toggle effect
+
+## Contributor (Developer, Designer)
+
+- [Hmabiola](https://hasbunallahmubarak.github.io/Hmabiola-Portfolio/)
+
+- [Tooplate](https://tooplate.com)
---
+++ js/kool_pack.js
@@ -0,0 +1,95 @@
+const modal = document.getElementById("myModal");
+const content = document.getElementById("myModal-container");
+const menubar = document.getElementById("menu-bar");
+const closeBtn = document.getElementById("close");
+const formNotify = document.getElementById("notifyBtn");
+let overlay = document.getElementById("overlay");
+const sidebar =  document.getElementById("side-bar");
+
+//Top menu bar side-bar function
+menubar.addEventListener("click", sideBarFunction);
+function sideBarFunction() {
+    sidebar.style.right = "0px";
+    overlay.style.display = "block";
+}
+
+//Close side-bar functions
+closeBtn.addEventListener("click", closeSideBar);
+overlay.addEventListener("click",  closeSideBar);
+overlay.addEventListener("click",  closeTheModal);
+function closeSideBar() {
+    if (sidebar.style.right === "0px") {
+        sidebar.style.right = "-500px";
+        overlay.style.display = "none";
+    };
+    if (modal.style.display === "block") {
+        overlay.style.display = "block";
+    } else {
+        overlay.style.display = "none";
+    }  
+}
+
+const notifyBtn = document.querySelector("#notify");
+const closeModal = document.getElementById("closeModal");
+
+notifyBtn.addEventListener("click", () => {
+    if (modal.style.display === "none") {
+       modal.style.display = "block";
+       modal.className = "addAnim";
+       overlay.style.display = "block";
+    } else {
+        closeTheModal();
+    }
+});
+
+closeModal.addEventListener("click", closeTheModal);
+formNotify.addEventListener("click", closeTheModal);
+
+function closeTheModal() {
+    modal.className = "removeAnim";
+    modal.style.display = "none";
+    overlay.style.display = "none";
+}
+
+//Count-down function
+const countDownContainer = document.querySelector("#count-down");
+let countContainer = document.createElement("ul");
+let interval = setInterval(() => {
+    let days, hours, minutes, seconds;
+    const currentDate = new Date();
+    let newDate = new Date("Dec 07 2024 11:59:59");
+    let difference =  newDate - currentDate;
+    days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((difference % (1000 * 60 )) / (1000));
+    const countDownObj = {
+        first: days,
+        second: hours,
+        third: minutes, 
+        fourth: seconds
+    };
+    viewInDom(countDownObj);
+}, 1000);
+function viewInDom(obj) {
+    const {first, second, third, fourth} = obj;
+    countContainer.innerHTML = `
+        <li class = "counter-item">
+            <h3>${first}</h3>
+            <p> Days </p>
+        </li>
+        <li class = "counter-item">
+            <h3>${second}</h3>
+            <p>Hours</p>
+        </li>
+        <li class = "counter-item">
+            <h3>${third}</h3>
+            <p>Minutes</p>
+        </li>
+        <li class = "counter-item">
+            <h3>${fourth}</h3>
+            <p> Seconds </p>
+        </li>
+    `;
+}
+countDownContainer.appendChild(countContainer);
